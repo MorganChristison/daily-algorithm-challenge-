fd48efa4a9ea24e18f69006235a3a4ef76a02997
Morgan <morgan.christison@gmail.com>
2016-09-20 19:49:20 -0700

    test for bubblesort passing

--- test/testing.js
+++ test/testing.js
@@ -1,3 +1,8 @@
 var test = require('tape');
+var bubbleSort = require('./../challenges/challenge1');
 
-test('test bubblesort', function())
+test('test bubblesort', function(t){
+	t.deepEqual(bubbleSort([1,4,5,2,3]), [1,2,3,4,5]);
+	t.end();
+}); 
+
