de1a53f2d24d5f06400d252cc0d03d4654e87541
Morgan <morgan.christison@gmail.com>
2016-09-22 21:24:00 -0700

    selsection sort passing

--- test/testing.js
+++ test/testing.js
@@ -1,8 +1,14 @@
 var test = require('tape');
 var bubbleSort = require('./../challenges/challenge1');
+var selectionSort = require('./../challenges/challenge2');
 
 test('test bubblesort', function(t){
 	t.deepEqual(bubbleSort([1,4,5,2,3]), [1,2,3,4,5]);
 	t.end();
 }); 
+
+test('test selsectionsort', function(t){
+	t.deepEqual(selectionSort([1,4,5,2,3]), [1,2,3,4,5]);
+	t.end();
+});
 
