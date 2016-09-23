292b5dda1444cb447fbc674297d932389fca7545
Morgan <morgan.christison@gmail.com>
2016-09-22 21:18:16 -0700

    selection sort complete

--- challenges/challenge2.js
+++ challenges/challenge2.js
@@ -5,3 +5,19 @@
 // Now go through the rest of the array (excluding the first element) and find the index of the lowest and swap it with the second element.
 
 // thats how it continues to select (find out) the lowest element of the array and putting it on the left until it hits the last element.
+function selectionSort(arr) {
+  var minIdx, temp, 
+      len = arr.length;
+  for(var i = 0; i < len; i++) {
+    minIdx = i;
+    for(var  j = i+1; j<len; j++) {
+       if(arr[j]<arr[minIdx]){
+          minIdx = j;
+       }
+    }
+    temp = arr[i];
+    arr[i] = arr[minIdx];
+    arr[minIdx] = temp;
+  }
+  return arr;
+}
