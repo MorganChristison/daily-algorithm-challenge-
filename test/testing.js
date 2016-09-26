e4faadf7acd1963df1dce875b5e7fc30f25fde5c
Morgan <morgan.christison@gmail.com>
2016-09-25 19:16:39 -0700

    merge sort testing

--- test/testing.js
+++ test/testing.js
@@ -1,6 +1,7 @@
 var test = require('tape');
 var bubbleSort = require('./../challenges/challenge1');
 var selectionSort = require('./../challenges/challenge2');
+var mergeSort = require('./../challenges/challenge3');
 
 test('test bubblesort', function(t){
 	t.deepEqual(bubbleSort([1,4,5,2,3]), [1,2,3,4,5]);
@@ -9,6 +10,11 @@ test('test bubblesort', function(t){
 
 test('test selsectionsort', function(t){
 	t.deepEqual(selectionSort([1,4,5,2,3]), [1,2,3,4,5]);
+	t.end();
+});
+
+test('test mergesort', function(t){
+	t.deepEqual(mergeSort([1,3,2,5,4]), [1,2,3,4,5]);
 	t.end();
 });
 
