165e3bedd3c963952b56906bb211d4b6028c0950
Morgan <morgan.christison@gmail.com>
2016-09-27 23:24:18 -0700

    quick sort solved

--- challenges/challenge4.js
+++ challenges/challenge4.js
@@ -7,3 +7,43 @@
 
 // Step-3:Repeat the step-2 for both left and right side of the pivot 
 // (pick a pivot, put all item smaller than the pivot to the left and larger on the right)
+
+function quickSort(arr, left, right){
+   var len = arr.length, 
+   pivot,
+   partitionIndex;
+
+
+  if(left < right){
+    pivot = right;
+    partitionIndex = partition(arr, pivot, left, right);
+    
+   //sort left and right
+   quickSort(arr, left, partitionIndex - 1);
+   quickSort(arr, partitionIndex + 1, right);
+  }
+  return arr;
+}
+        
+
+function partition(arr, pivot, left, right){
+   var pivotValue = arr[pivot],
+       partitionIndex = left;
+
+   for(var i = left; i < right; i++){
+    if(arr[i] < pivotValue){
+      swap(arr, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(arr, right, partitionIndex);
+  return partitionIndex;
+}
+        
+
+function swap(arr, i, j){
+   var temp = arr[i];
+   arr[i] = arr[j];
+   arr[j] = temp;
+}
+     
