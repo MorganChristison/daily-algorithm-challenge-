515f22df9b6e2c31858b406b4dbbf65100be133f
Morgan <morgan.christison@gmail.com>
2016-09-27 23:22:54 -0700

    quick sort algorithm pseudo-code

--- challenges/challenge4.js
+++ challenges/challenge4.js
@@ -1,1 +1,9 @@
 //quick sort
+// Step-1: You have to pick a pivot. This could be randomly selected 
+// or the middle one. Here we select the last element of the array.
+
+// Step-2: Put all the items smaller than the pivot value to the left 
+// and larger than the pivot value to the right.
+
+// Step-3:Repeat the step-2 for both left and right side of the pivot 
+// (pick a pivot, put all item smaller than the pivot to the left and larger on the right)
