b453359cb528bccf799c5bad15cec059b30e05e7
Morgan <morgan.christison@gmail.com>
2016-10-11 20:54:50 -0700

    all tests passing

--- test/testing.js
+++ test/testing.js
@@ -3,7 +3,7 @@ var bubbleSort = require('./../challenges/challenge1');
 var selectionSort = require('./../challenges/challenge2');
 var mergeSort = require('./../challenges/challenge3');
 var quickSort = require('./../challenges/challenge4');
-var heapSort = require('./..challenges/challenge5');
+var heapSort = require('./../challenges/challenge5');
 
 test('test bubblesort', function(t){
 	t.deepEqual(bubbleSort([1,4,5,2,3]), [1,2,3,4,5]);
@@ -22,5 +22,9 @@ test('test mergesort', function(t){
 
 test('test quickSort', function(t){
 	t.deepEqual(quickSort([1,3,2,5,4],0,4), [1,2,3,4,5]);
+	t.end();
+});
+test('test heapSort', function(t){
+	t.deepEqual(heapSort([1,3,2,5,4],0,4), [1,2,3,4,5]);
 	t.end();
 });
